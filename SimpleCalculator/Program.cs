63b85a08ceacf7fe889b222f2e08d9e4ff6deafa
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2019-02-20 10:57:23 +0100

    adding input text - program completed

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -14,12 +14,15 @@ namespace SimpleCalculator
             InputConverter inputConverter = new InputConverter();
             CalculatorEngine calculatorEngine = new CalculatorEngine();
 
+            Console.Write("Enter a number: ");
             double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+            Console.Write("Enter another number: ");
             double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+            Console.Write("Enter an operation(+, -, * or /): ");
             string operation = Console.ReadLine();
 
             double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
-            Console.WriteLine(result);
+            Console.WriteLine("The result of {0} {1} {2} is {3}", firstNumber, operation, secondNumber, result);
             Console.ReadKey();
         }
     }
