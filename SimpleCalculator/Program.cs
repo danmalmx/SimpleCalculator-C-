0055a1a22ff849a1b67dc12a303728707a6173ef
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2019-02-20 10:37:18 +0100

    adding InputConverter class and convertedNumber method

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,6 +10,17 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+
+            InputConverter inputConverter = new InputConverter();
+            CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+            double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+            double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+            string operation = Console.ReadLine();
+
+            double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
