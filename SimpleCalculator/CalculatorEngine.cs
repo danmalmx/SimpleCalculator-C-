16db66c42a09a6e7f5cd288fa43582a81f82ac72
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2019-02-20 10:47:02 +0100

    adding CalculatorEngine class and Calculate method

---
+++ SimpleCalculator/CalculatorEngine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class CalculatorEngine
+    {
+        public double Calculate (string Opertation, double FirstNumber, double SecondNumber)
+        {
+            double result;
+        
+            switch (Opertation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = FirstNumber + SecondNumber;
+                    break;
+                case "subtract":
+                case "-":
+                    result = FirstNumber - SecondNumber;
+                    break;
+                case "multiply":
+                case "*":
+                    result = FirstNumber * SecondNumber;
+                    break;
+                case "divide":
+                case "/":
+                    result = FirstNumber / SecondNumber;
+                    break;
+                default:
+                    throw new InvalidOperationException("Specified operation is not recognized");
+            }
+            return result;
+        }
+    }
+}
